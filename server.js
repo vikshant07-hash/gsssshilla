63c88747cd59fb08787a231d8bcd3638c83d16cd
Vikshant <magicalmathsquiz@gmail.com>
2026-06-15 18:01:48 +0530

    fix nodemailer import

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const nodemailer = require("nodemailer");
+
 const app = express();
 const verifyToken =
   require("./middleware/authMiddleware");
