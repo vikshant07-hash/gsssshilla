db00d7e23c6e39bda4ea4dd955bbe523e2142f1f
Vikshant <magicalmathsquiz@gmail.com>
2026-06-15 18:05:22 +0530

    fix port

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const jwt = require("jsonwebtoken");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.BREVO_EMAIL,
