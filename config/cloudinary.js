f14795b0c0b852fb29195da7f768c5ff12dbdbd0
vikshant07-hash <magicalmathsquiz@gmail.com>
2026-08-06 20:33:41 +0530

    new update

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -12,6 +12,8 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+console.log("☁️ Cloudinary configured for:", process.env.CLOUDINARY_CLOUD_NAME);
+
 // ============================================================
 // STORAGE FOR SLIDER IMAGES
 // ============================================================
