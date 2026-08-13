4eb36324960e292e624041de1b487fccb4b2ef14
vikshant07-hash <magicalmathsquiz@gmail.com>
2026-08-13 21:37:03 +0530

    new update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
+const session = require('express-session'); 
 const fs = require("fs-extra");
 const { cloudinary, uploadSlider, uploadRecent, uploadGallery, uploadFaculty, uploadDownload } = require("./config/cloudinary");
 const { db } = require("./config/db");
